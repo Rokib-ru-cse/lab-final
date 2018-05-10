7360cd27a9963944956a7eccab0c1edd243183a1
Golamrabbihridoy <hridoyrabbi97@gmail.com>
2018-05-09 22:59:56 -0700

    SUM FUNCTION ADDED

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void sum(int data_1,int data_2)
+{
+      int  sum;
+      sum=data_1+data_2;
+      cout<<data_1<<" + "<<data_2<<" = "<<sum<<endl;
+}
+
 int main()
 {
 	int value_1,value_2;
@@ -7,6 +15,7 @@ int main()
 	cin>>value_1;
 	cout<<"Enter any value: ";
 	cin>>value_2;
+	sum(value_1,value_2);
 	return 0;
 }
 
